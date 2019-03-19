ec1a87e7fbcc677b13fc4f99958042a851f70a8c
Matheus Inácio <matheusinacio@Matheuss-MacBook-Pro-2.local>
2019-03-19 15:37:13 -0300

    upload

--- README.md
+++ README.md
@@ -7,26 +7,26 @@ To use JCL Host for Arduino, download the folder "libaries" and paste its conten
 #include <jcl.h>
 
 void setup() {
-  Serial.begin(9600); 
-
-  char hostIP[] = "192.168.1.199",
-        hostMAC[] = "AA-BB-CC-DD-EE-FF",        
-        serverIP[] = "192.168.1.16";
-
+  Serial.begin(9600);
+  char  hostMAC[] = "BA-DB-CD-DD-EE-FF",
+        serverIP[] = "192.168.1.19";
+  char * nameBoard = "arduino";
   int hostPort = 5151,
       serverPort = 6969;
 
-  JCL jcl(hostIP, hostPort, hostMAC);
+  JCL jcl(hostPort, hostMAC);
   jcl.configureJCLServer(serverIP, serverPort);
-  jcl.setEncryption(false); // optional (Default = false)
-  jcl.changeBoardNickname("arduino"); // optional (Board Nickname)
+  jcl.set_encryption(false); // optional (Default = false)
+  jcl.changeBoardNickname(nameBoard); // optional (Board Nickname)
   jcl.useEEPROM(false);  // optional (default = true) if false Arduino won't restore the configuration of all sensors and contexts when a reboot occurs
-  jcl.setBrokerData(serverIP, 1883);	// mqtt broker configuration
+  jcl.set_brokerData(serverIP, 1883);  // mqtt broker configuration
   jcl.startHost();
 }
 
 // no code is necessary in this method
 void loop() {
+
 }
+
 ```
 
--- example/example.ino
+++ example/example.ino
@@ -1,21 +1,20 @@
 #include <jcl.h>
 
 void setup() {
-    Serial.begin(9600);
-    char hostMAC[] = "BA-DB-CD-DD-EE-FF",
-            serverIP[] = "192.168.1.19";
-    char * name = "arduino";
+  Serial.begin(9600);
+  char  hostMAC[] = "BA-DB-CD-DD-EE-FF",
+        serverIP[] = "192.168.1.19";
+  char * nameBoard = "arduino";
+  int hostPort = 5151,
+      serverPort = 6969;
 
-    int hostPort = 5151,
-            serverPort = 6969;
-
-    JCL jcl(hostPort, hostMAC);
-    jcl.configureJCLServer(serverIP, serverPort);
-    jcl.set_encryption(false); // optional (Default = false)
-    jcl.changeBoardNickname(name); // optional (Board Nickname)
-    jcl.useEEPROM(false);  // optional (default = true) if false Arduino won't restore the configuration of all sensors and contexts when a reboot occurs
-    jcl.set_brokerData(serverIP, 1883);  // mqtt broker configuration
-    jcl.startHost();
+  JCL jcl(hostPort, hostMAC);
+  jcl.configureJCLServer(serverIP, serverPort);
+  jcl.set_encryption(false); // optional (Default = false)
+  jcl.changeBoardNickname(nameBoard); // optional (Board Nickname)
+  jcl.useEEPROM(false);  // optional (default = true) if false Arduino won't restore the configuration of all sensors and contexts when a reboot occurs
+  jcl.set_brokerData(serverIP, 1883);  // mqtt broker configuration
+  jcl.startHost();
 }
 
 // no code is necessary in this method
